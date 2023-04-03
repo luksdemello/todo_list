6386570cbb5c3a0d02c55a319109a7be5b85976d
Lucas de mello <lucas_mello6@hotmail.com>
2023-04-03 17:21:35 -0300

    feat: create register page

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -2,15 +2,19 @@ import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_module.dart';
 import 'package:todo_list/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list/app/modules/auth/login/login_page.dart';
+import 'package:todo_list/app/modules/auth/register/register_controller.dart';
+import 'package:todo_list/app/modules/auth/register/register_page.dart';
 
 class AuthModule extends TodoListModule {
   AuthModule()
       : super(
           routers: {
             '/login': (context) => const LoginPage(),
+            '/register': (context) => const RegisterPage(),
           },
           bindings: [
             ChangeNotifierProvider(create: (_) => LoginController()),
+            ChangeNotifierProvider(create: (_) => RegisterController()),
           ],
         );
 }
