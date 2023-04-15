ae85fedd2207b42743a768961b5612877dc2f5e6
Lucas de mello <lucas_mello6@hotmail.com>
2023-04-15 19:44:40 -0300

    feat: create layout home tasks component

---
+++ lib/app/modules/home/widgets/task.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Task extends StatelessWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      child: IntrinsicHeight(
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(0),
+          leading: Checkbox(
+            onChanged: (value) {},
+            value: true,
+          ),
+          title: const Text(
+            'Descrição da task',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          subtitle: const Text(
+            '15/04/2023',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(width: 1),
+          ),
+        ),
+      ),
+    );
+  }
+}
